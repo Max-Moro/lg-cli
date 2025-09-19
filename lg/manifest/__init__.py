36476cadfdd646252d8e12481aebaab3464153ab
Maxim Morozov <mmocentre@gmail.com>
2025-09-19 20:30:39 +0500

    Удаляем старый построитель манифестов

--- lg/manifest/__init__.py
+++ lg/manifest/__init__.py
@@ -1,7 +1,3 @@
-from .builder import build_manifest
 from .builder_v2 import build_section_manifest
 
-__all__ = [
-    "build_manifest",
-    "build_section_manifest",
-]
+__all__ = ["build_section_manifest"]
