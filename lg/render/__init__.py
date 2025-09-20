ea8ed7be40368888ece4367b37a1bab6833e67d2
Maxim Morozov <mmocentre@gmail.com>
2025-09-20 09:37:57 +0500

    Удаляем старый `lg/render/renderer.py`

--- lg/render/__init__.py
+++ lg/render/__init__.py
@@ -1,5 +1,3 @@
-from .renderer import render_document
 from .renderer_v2 import render_section, render_document_v2
-from .sections import render_by_section
 
-__all__ = ["render_document", "render_document_v2", "render_section", "render_by_section"]
+__all__ = ["render_document_v2", "render_section"]
