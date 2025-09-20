7a73e32a86eaf8d4088c20dd2c43771c1308f9c7
Maxim Morozov <mmocentre@gmail.com>
2025-09-20 09:29:18 +0500

    Удаляем `lg/plan/planner.py`

--- lg/plan/__init__.py
+++ lg/plan/__init__.py
@@ -1,4 +1,3 @@
 from __future__ import annotations
-from .planner import build_plan
 from .planner_v2 import build_section_plan
-__all__ = ["build_plan", "build_section_plan"]
+__all__ = ["build_section_plan"]
