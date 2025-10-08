387af57b15495308733f935cbfd7efc0e0723ead
Maxim Morozov <mmocentre@gmail.com>
2025-10-08 15:02:27 +0500

    Исправление импортов в `lg/stats/__init__.py`

--- lg/stats/__init__.py
+++ lg/stats/__init__.py
@@ -5,11 +5,15 @@ from .tokenizers import (
     list_encoders,
 )
 
+from .tokenizer import TokenService
 from .collector import StatsCollector
 from .report_builder import build_run_result_from_collector
 from .report_schema import RunResult
 
 __all__ = [
+    # TokenService
+    "TokenService",
+
     # Tokenizers
     "BaseTokenizer",
     "create_tokenizer",
