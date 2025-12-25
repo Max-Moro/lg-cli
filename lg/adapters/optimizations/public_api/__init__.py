cfc2332f67a7c541fdfa8477a3251400941f974b
Maxim Morozov <mmocentre@gmail.com>
2025-12-26 02:07:08 +0500

    Cleaning up the `lg/adapters/optimizations/public_api/*` module

--- lg/adapters/optimizations/public_api/__init__.py
+++ lg/adapters/optimizations/public_api/__init__.py
@@ -4,9 +4,5 @@ Infrastructure for declarative element collection and filtering.
 """
 
 from .optimizer import PublicApiOptimizer
-from .profiles import LanguageElementProfiles
 
-__all__ = [
-    "PublicApiOptimizer",
-    "LanguageElementProfiles"
-]
+__all__ = ["PublicApiOptimizer"]
